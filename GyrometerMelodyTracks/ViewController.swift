d220bedd5a196b0567cbf9b0736569a35fbafb06
John Baer <johnbaer3@gmail.com>
2020-07-01 13:47:56 -0700

    Initial Commit

---
+++ GyrometerMelodyTracks/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GyrometerMelodyTracks
+//
+//  Created by John Baer on 7/1/20.
+//  Copyright © 2020 John Baer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
